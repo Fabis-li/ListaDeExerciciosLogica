75e61ae96849bf01525b7a84e93a42d55012287b
Fabis-li <fabiano_slima@hotmail.com>
2024-03-14 17:44:22 -0300

    Resolucao Exercicio2

---
+++ Exercicio2.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Converter temperatura gruas Fahrenheit para graus Celsius\n");
+
+            Console.WriteLine("Digite a temperatura em Fahrenheit");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            
+            double celsisus = (fahrenheit - 32) * 5 / 9;            
+
+            Console.WriteLine("A temperatura em convertida de Fahrenheit para Celsius é: " + celsisus + " graus");
+            Console.ReadLine();
+
+        }
+    }
+}
