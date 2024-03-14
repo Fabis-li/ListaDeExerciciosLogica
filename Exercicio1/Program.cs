cf3a36d80e4a21ae05bf9ac58386a6734713bac0
Fabis-li <fabiano_slima@hotmail.com>
2024-03-14 15:40:36 -0300

    Add project files.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular o volume de uma caixa retangular\n");
+            
+            Console.WriteLine("Digite um comprimento");
+            double comprimento = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a largura");
+            double largura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double volume = comprimento * largura * altura;
+
+            Console.WriteLine("O volume da caixa retangular é: " + volume);
+
+
+
+
+        }
+    }
+}
---
+++ ListaDeExerciciosLogica.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaDeExerciciosLogica.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
