93c2ffcfc666475cfa00f3b10017c96b2e855d5c
Fabis-li <fabiano_slima@hotmail.com>
2024-03-14 19:47:09 -0300

    Resolucao exercicio 3

---
+++ Exercicio3.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular volume de um cilindro\n");
+
+            Console.WriteLine("Digite o raio da cilindro");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do cilindro");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double PI = 3.14;
+
+            double volume = PI * (raio * raio) * altura;
+
+            Console.WriteLine("O Volume do Cilindro é: " + Math.Round(volume,2));  
+            Console.ReadLine();
+        }
+    }
+}
