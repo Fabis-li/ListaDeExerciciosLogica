31fe8c448fb64b10741eb2ba39bb343969823184
Fabis-li <fabiano_slima@hotmail.com>
2024-03-15 15:34:37 -0300

    Resolucao exercicio 7

---
+++ Exercicio7.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio7.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular Salario Total de Vendedor\n");
+            Console.WriteLine("Informe o salario base em reais");
+            decimal salarioBase = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe o total das vendas em reais");
+            decimal totalVendas = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe o percentual de comissão");
+            decimal comissao = Convert.ToDecimal(Console.ReadLine());
+
+            decimal salarioTotal = salarioBase + (totalVendas * comissao / 100);
+
+            Console.WriteLine(" O salario total do vendedor e R$: " + Math.Round(salarioTotal, 2));
+            Console.ReadLine();
+
+        }
+    }
+}
