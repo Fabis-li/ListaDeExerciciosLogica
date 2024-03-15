2b7f7c1ce0b9d3fe5f5b213647f3c081669eea06
Fabis-li <fabiano_slima@hotmail.com>
2024-03-14 21:59:27 -0300

    Resolucao exercicio 5

---
+++ Exercicio5.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio5.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo do volumo da esfera");
+            Console.WriteLine("Informe o tamanho do raio");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            double PI = 3.14;
+
+            double volume = 4 / 3 * PI * raio;
+
+            Console.WriteLine("O volume da esfera é : " + Math.Round(volume,2));
+            Console.ReadLine();
+        }
+    }
+}
