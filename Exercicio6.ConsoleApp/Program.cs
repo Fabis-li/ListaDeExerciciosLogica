40c22dcdc60cf33ec04fa92c6b25180f54f65f42
Fabis-li <fabiano_slima@hotmail.com>
2024-03-14 22:10:14 -0300

    Resolucao exercicio 6

---
+++ Exercicio6.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio6.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo para converter Celsius para Fahrenheit");
+            Console.WriteLine("Digite a temperatura");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius + 32) * 9 / 5;
+
+            Console.WriteLine("A temperatura em graus Fahrenheit é: " + Math.Round(fahrenheit, 2));
+            Console.ReadLine();
+        }
+    }
+}
