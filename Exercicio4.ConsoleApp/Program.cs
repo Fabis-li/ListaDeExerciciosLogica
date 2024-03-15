332e3513500fae629a78653e91b4b6638daf21e7
Fabis-li <fabiano_slima@hotmail.com>
2024-03-14 21:40:45 -0300

    Resolucao exercicio 4

---
+++ Exercicio4.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio4.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de consumo de combustivel por Km");
+            Console.WriteLine("Informe a kilometragem inicial");
+            decimal kmInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe a Kilometragem final");
+            decimal kmFinal = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe o consumo de combustivel");
+            decimal consumoCombustivel = Convert.ToDecimal(Console.ReadLine());
+
+            decimal consumoPorKm = (kmFinal - kmInicial) / consumoCombustivel;
+
+            Console.WriteLine("O consumo de combustivel por km è : " + consumoPorKm + " litros por km rodado");
+        }
+    }
+}
