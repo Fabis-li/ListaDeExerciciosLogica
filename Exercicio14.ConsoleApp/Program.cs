7793131e5e4cc06706f23a721c1ce330d0e92a62
Fabis-li <fabiano_slima@hotmail.com>
2024-03-16 17:02:19 -0300

    Resultado exercicio 14

---
+++ Exercicio14.ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Exercicio14.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Calcular dias de vida\n");
+
+                Console.WriteLine("Digite o seu nome");
+                string nome = Console.ReadLine();
+
+                if(string.IsNullOrWhiteSpace(nome))
+                {
+                    Console.WriteLine("Digite apenas letras");
+                    Console.WriteLine("Digite o seu nome");
+                    nome = Console.ReadLine();
+                }                
+
+                Console.WriteLine("Digite a sua idade");
+                string idade = Console.ReadLine();
+
+                if(!int.TryParse(idade, out _))
+                {
+                    Console.WriteLine("Insira uma idade válida");
+                    Console.WriteLine("Digite sua idade");
+                    idade = Console.ReadLine();
+                }
+
+                int diasDeVida = Convert.ToInt32(idade) * 365;
+
+                Console.WriteLine(nome.ToUpper() + " ,VOCÊ JA VIVEU " + diasDeVida + " DIAS.\n");
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+                }
+
+            }
+        }
+    }
+}
