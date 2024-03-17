814bde92d5065bdb0b6a529d6b5bcf154fd2f6dd
Fabis-li <fabiano_slima@hotmail.com>
2024-03-17 19:39:08 -0300

    Resolucao exercicio 24

---
+++ Exercicio24.ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Exercicio24.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Tabuada\n");
+
+                Console.WriteLine("Digite um numero entre 1 e 10");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if(n < 0 || n > 10)
+                {
+                    Console.WriteLine("Digite um número entre 1 e 10");
+                    n = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int contador = 0;
+                int resultado = 0;
+
+                while( contador <= 10)
+                {
+                    resultado = contador * n;
+                    Console.WriteLine(contador + " x " + n + " = " + resultado);                    
+                    contador++;
+                }
+
+                Console.ReadLine();
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+                }
+
+
+            }
+        }
+    }
+}
