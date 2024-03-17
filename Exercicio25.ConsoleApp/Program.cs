497aaa57fdd0eb25bdd9b1b73efa1a91e02464ed
Fabis-li <fabiano_slima@hotmail.com>
2024-03-17 20:28:08 -0300

    Resolucao exercicio 25

---
+++ Exercicio25.ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercicio25.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Fatorial");
+
+                Console.WriteLine("Digite um valor");
+                int valor = Convert.ToInt32(Console.ReadLine());
+
+                //5 = 5x4x3x2x1 = 120
+
+                int resultado = 1;
+                int contador = valor;
+
+                Console.WriteLine(valor + "! = ");
+
+                while(contador > 0)
+                {
+                    resultado *= contador;
+                    Console.WriteLine(contador);
+                    if( contador != 1)
+                    {
+                        Console.WriteLine(" x ");
+                    }
+                    contador--;                    
+                }
+                Console.WriteLine(" = " + resultado);
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+                }
+
+
+
+            }
+        }
+    }
+}
