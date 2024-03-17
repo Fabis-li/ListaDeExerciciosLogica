a9a5a0a0857a7eeb374a4bb5dc414609fdbf4913
Fabis-li <fabiano_slima@hotmail.com>
2024-03-17 19:17:15 -0300

    Resolucao exercicio 23

---
+++ Exercicio23.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercicio23.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Somar os numeros ímpares que são multiplos de 3\n");
+
+                int numero = 1;
+                int soma = 0;
+
+                while(numero < 500)
+                {                    
+                    if(numero % 2 != 0 && numero % 3 == 0)
+                    {
+                        Console.WriteLine(soma);
+                        soma += numero;
+                    }
+                    numero++;                 
+                }
+               
+                Console.ReadLine(); 
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+                }
+
+
+            }
+        }
+    }
+}
