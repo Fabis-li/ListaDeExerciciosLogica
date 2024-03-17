ba5f5bc7b34c539ab5df9d1a1863629a3ee25d12
Fabis-li <fabiano_slima@hotmail.com>
2024-03-17 14:55:08 -0300

    Resolucao exercicio 21

---
+++ Exercicio21.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Exercicio21.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Lendo 2 valores, se forem iguais soma e for diferente multiplica");
+                Console.WriteLine("Digite um valor para A");
+                int A = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite um valor para B");
+                int B = Convert.ToInt32(Console.ReadLine());
+
+                int soma = A + B;
+                int multiplacao = A * B;
+                int C = 0;
+
+                if(A == B)
+                {
+                    C = soma;
+                    Console.WriteLine("A e B são iguais, a soma é : " + C + " \n");
+                }
+                else
+                {
+                    C = multiplacao;
+                    Console.WriteLine("A e B são diferentes, o resultado da multiplicação entre eles é : " + C + " \n");
+                }
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+                }
+
+            }
+        }
+    }
+}
