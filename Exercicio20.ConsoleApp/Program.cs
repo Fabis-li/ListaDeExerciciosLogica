bd371fa746c2a353bbe575a42d9a875f32972494
Fabis-li <fabiano_slima@hotmail.com>
2024-03-17 14:54:46 -0300

    ajuste

---
+++ Exercicio20.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercicio20.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Par ou Ímpar\n");
+                Console.WriteLine("Digite um número");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                int resto = numero % 2;
+
+                if(resto == 0 )
+                {
+                    Console.WriteLine("O número digita é par.\n");
+                }
+                else
+                {
+                    Console.WriteLine("O número digitado é ímpar.\n");
+                }
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+                }
+
+
+
+
+            }
+        }
+    }
+}
