e93b177f8af29428cd700a9306e8806165806e2f
Fabis-li <fabiano_slima@hotmail.com>
2024-03-17 23:43:41 -0300

    Resolucao exercicio 27

---
+++ Exercicio27.ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Exercicio27.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("FizzBuzz");
+                Console.WriteLine("Imprimindo números na tela");
+
+                int inicial = 1;
+                int limite = 100;
+
+                while(inicial<=limite)
+                {
+                    //Console.WriteLine(inicial);
+                    //inicial++;
+                    if(inicial % 3 == 0 && inicial % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if ( inicial % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (inicial % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(inicial);
+                    }
+                    inicial++;
+                }
+
+                Console.ReadLine();
+
+                Console.WriteLine("Digite S para sair e N para voltar ao inicio");
+
+                String sair = Console.ReadLine();
+
+                if (sair == "S")
+                {
+
+                    break;
+
+                }
+                if (sair == "N")
+                {
+
+                    continue;
+                }
+
+            }
+        }
+    }
+}
