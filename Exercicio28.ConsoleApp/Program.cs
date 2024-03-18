958de7a40105e7ab875594604a5249a40b7b528b
Fabis-li <fabiano_slima@hotmail.com>
2024-03-18 00:16:22 -0300

    Resolucao exercicio 28

---
+++ Exercicio28.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercicio28.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Tabuada em coluna");
+
+                int numero = 1;
+
+                while(numero <= 10)
+                {
+                    int numero2 = 1;
+
+                    while( numero2 <= 10)
+                    {
+                        int resultado = numero * numero2;
+
+                        Console.WriteLine(numero + " x " + numero2 + " = " + resultado + "\t");
+
+                        numero2++;
+
+                        if(numero == 11)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                    numero++;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
